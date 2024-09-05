c278a5bfb51e1b518edfec2df321bfeed88ae716
Feedmon <49274728+Feedmon@users.noreply.github.com>
2024-09-05 23:51:02 +0200

    added a check for java service improvement wait for java service and client connection

--- src/main/java/feedmon/testing/adapters/rest/Controllers/LCUController.java
+++ src/main/java/feedmon/testing/adapters/rest/Controllers/LCUController.java
@@ -8,7 +8,13 @@ import feedmon.testing.usecases.GetLCUConnectionStatusUseCase;
 import feedmon.testing.usecases.dtos.SpecialChallengesDto;
 import generated.LolChampionsCollectionsChampionSkin;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
